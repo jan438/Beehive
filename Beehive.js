b7ead59d550d883851eea3c01454616c041de4f4
Jan Boon <janboon438@gmail.com>
2016-10-17 13:36:47 +0200

    Button replaced by an image

--- Beehive.js
+++ Beehive.js
@@ -4,23 +4,7 @@ var transform = prefix('transform');
 var translate = Deck.translate;
 var $container = document.getElementById('container');
 var $topbar = document.getElementById('topbar');
-var $Beehive = document.createElement('button');
-$Beehive.textContent = 'Beehive';
-$topbar.appendChild($Beehive);
+$("#topbar").hide();
 var deck = Deck();
-$Beehive.addEventListener('click', function () {
-  deck.queue(function (next) {
-    deck.cards.forEach(function (card, i) {
-      setTimeout(function () {
-        card.setSide('back')
-      }, i * 7.5)
-    })
-    next()
-  })
-  deck.shuffle()
-  deck.shuffle()
-  deck.sort()
-  deck.Beehive()
-});
 deck.mount($container);
 deck.intro();
